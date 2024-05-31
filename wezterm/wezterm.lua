af7f96307d4feed51f87e9fd5604c5d5d27945bc
MatthewBotha <m.botha41@gmail.com>
2024-05-31 18:05:32 +0200

    wezterm tweeking

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,13 +3,24 @@ local wezterm = require 'wezterm'
 local config = {}
 
 -- color scheme
--- config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'Catppuccin Mocha'
 
 -- background image
 config.window_background_image = '/home/lemonsir/.config/wezterm/glass-key.png'
+-- config.window_background_image = '/home/lemonsir/.config/wezterm/nausicaa.jpg'
 config.window_background_image_hsb = {
-    -- darken the background image
-    brightness = 0.3,
+    -- hue
+    hue = 1.0,
+
+    -- saturation
+    saturation = 1.0,
+
+    -- brightness
+    brightness = 0.05,
 }
+
+-- fonts
+config.font = wezterm.font 'DM Mono'
+config.font_size = 11.0
 
 return config
