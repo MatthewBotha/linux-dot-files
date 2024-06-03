3e3930a977b8dfc3e192e96da03d764a5e01226a
MatthewBotha <m.botha41@gmail.com>
2024-06-03 14:51:44 +0200

    wezterm formatting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,26 +1,49 @@
 -- WezTerm Configuration
 local wezterm = require 'wezterm'
 local config = {}
+local mux = wezterm.mux
 
 -- color scheme
 config.color_scheme = 'Catppuccin Mocha'
 
 -- background image
-config.window_background_image = '/home/lemonsir/.config/wezterm/glass-key.png'
--- config.window_background_image = '/home/lemonsir/.config/wezterm/nausicaa.jpg'
-config.window_background_image_hsb = {
-    -- hue
-    hue = 1.0,
+config.background = {
+    {
+        source = {
+            File = '/home/lemonsir/.config/wezterm/glass-key.png',
+        },
+        hsb = { brightness = 0.1 },
 
-    -- saturation
-    saturation = 1.0,
-
-    -- brightness
-    brightness = 0.05,
+        -- position and scale image correctly
+        vertical_align = "Middle",
+        horizontal_align = "Center",
+        height = "Cover",
+        width = "Cover",
+    }
 }
 
 -- fonts
 config.font = wezterm.font 'DM Mono'
 config.font_size = 11.0
+
+-- title bar
+config.window_decorations = "NONE"
+
+-- tab bar
+config.enable_tab_bar = false
+
+-- padding
+config.window_padding = {
+    left    = "1cell",
+    right   = "1cell",
+    top     = "0.5cell",
+    bottom  = 0,
+}
+
+-- maximize on startup
+wezterm.on("gui-startup", function()
+    local tab, pane, window = mux.spawn_window{}
+    window:gui_window():maximize()
+end)
 
 return config
