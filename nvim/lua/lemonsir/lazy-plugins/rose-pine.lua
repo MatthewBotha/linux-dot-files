6acdb71295fdd32aa90113ed7fb2c10154827ce8
MatthewBotha <m.botha41@gmail.com>
2024-06-04 12:17:11 +0200

    neovim theming

--- nvim/lua/lemonsir/lazy-plugins/rose-pine.lua
+++ nvim/lua/lemonsir/lazy-plugins/rose-pine.lua
@@ -1,8 +1,76 @@
 return {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    config = function()
-      -- load the color scheme
-      -- vim.cmd([[colorscheme rose-pine]])
-    end,
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        dependencies = {
+            'nvim-telescope/telescope.nvim',
+        },
+        config = function()
+            -- load the color scheme
+            require('rose-pine').setup({
+                variant = 'moon', -- auto, main, moon, or dawn
+                dim_inative_windows = true,
+                extend_background_behind_borders = true,
+                highlight_groups = {
+                    TelescopeBorder = { fg = 'highlight_high', bg = 'none' },
+                    TelescopeNormal = { bg = 'none' },
+                    TelescopePromptNormal = { bg = 'none' },
+                    TelescopeResultsNormal = { fg = 'subtle', bg = 'none' },
+                    TelescopeSelection = { fg = 'text', bg = 'none' },
+                    TelescopeSelectionCaret = { fg = 'rose', bg = 'rose' },
+                }
+            })
+            vim.cmd('colorscheme rose-pine-moon')
+            
+            -- transparent background
+            vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+            vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+        end,
+    },
+    {
+        'maxmx03/roseline',
+        dependencies = {
+            'rose-pine/neovim',
+            'lewis6991/gitsigns.nvim',
+        },
+        config = function()
+            require('gitsigns').setup()
+            require('roseline').setup({
+                theme = 'rose-pine',
+                layout = {
+                    a = section_a,
+                    b = section_b,
+                    c = section_c,
+                    d = section_d,
+                    e = section_e,
+                },
+                icons = {
+                    vim = '',
+                    git = {
+                        head = '',
+                        added = '',
+                        changed = '',
+                        removed = '',
+                    },
+                    diagnostic = {
+                        Error = '',
+                        Warning = '',
+                        Information = '',
+                        Question = '',
+                        Hint = '󰌶',
+                        Debug = '',
+                        Ok = '󰧱',
+                    },
+                    os = {
+                        Linux = '',
+                        microsoft = '',
+                        Darwin = '',
+                    },
+                    default = { left = '', right = '' },
+                    block = { left = '█', right = '█' },
+                    round = { left = '', right = '' },
+                },
+            })
+        end,
+    }
 }
--- nvim/lua/lemonsir/lazy-plugins/telescope.lua
+++ nvim/lua/lemonsir/lazy-plugins/telescope.lua
@@ -1,12 +1,22 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" }, 
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' }, 
     config = function()
         local builtin = require('telescope.builtin')
+        require('telescope').setup({
+            highlight_groups = {
+                TelescopeBorder = { fg = 'highlight_high', bg = 'none' },
+                TelescopeNormal = { bg = 'none' },
+                TelescopePromptNormal = { bg = 'none' },
+                TelescopeResultsNormal = { fg = 'subtle', bg = 'none' },
+                TelescopeSelection = { fg = 'text', bg = "none" },
+            }
+        })
+
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") });
+            builtin.grep_string({ search = vim.fn.input('Grep > ') });
         end)
     end,
 }
