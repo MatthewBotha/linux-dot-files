6d3c339adeba7d336824f69b88a80c12a250b7d3
Matthew Botha <m.botha41@gmail.com>
2024-06-21 20:31:49 +0200

    put omz at the bottom

--- scripts/apt-install-script.sh
+++ scripts/apt-install-script.sh
@@ -47,6 +47,7 @@ PACKAGES=(
     git
     gh
     neovim
+    zsh
     # programs
     firefox
     # utils
@@ -71,9 +72,6 @@ PACKAGES=(
 )
 sudo apt install "${PACKAGES[@]}" -y
 
-# oh-my-zsh - https://ohmyz.sh/#install
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Git Credential Manager
 curl -L https://aka.ms/gcm/linux-install-source.sh | sh
 git-credential-manager configure
@@ -84,3 +82,6 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+
+# oh-my-zsh - https://ohmyz.sh/#install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
