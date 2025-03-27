79f802292231a6d35c2752f0933080e8bfaa01aa
Matthew Botha <botham@mit.edu>
2025-03-27 09:54:17 +0200

    comment out lazygit default tab (in nvim)

--- wez-setup.sh
+++ wez-setup.sh
@@ -8,7 +8,7 @@ fi
 
 # open lazygit and nvim if in git repo
 if git rev-parse --git-dir > /dev/null 2>&1; then
-    wezterm cli spawn --cwd="$1" lazygit
+    # wezterm cli spawn --cwd="$1" lazygit
     wezterm cli spawn --cwd="$1" nvim .
 fi
 
